081e55f515163cce9c21a54758b45dfc384ab75a
ramverma09 <rv638069@gmail.com>
2026-06-01 00:25:24 +0530

    using dynamic routing

--- src/app.js
+++ src/app.js
@@ -3,23 +3,24 @@ const express  = require('express');
 const app = express();
 
 
-app.get("/user", (req,res) =>{
+app.get("/user/:userId/:name/:password", (req,res) =>{
+    console.log(req.params);
     res.send({"firstName": "Ram", "lastName": "Verma"});
 });
 
-app.post("/user", (req,res) =>{
-    // Saved the data to database
-    res.send("Data saved successfully to the database");
-});
+// app.post("/user", (req,res) =>{
+//     // Saved the data to database
+//     res.send("Data saved successfully to the database");
+// });
 
-app.delete("/user", (req,res) =>{
-    // Delete the data from database
-    res.send("Data deleted successfully from the database");
-});
+// app.delete("/user", (req,res) =>{
+//     // Delete the data from database
+//     res.send("Data deleted successfully from the database");
+// });
 
-app.use( "/test", (req,res) =>{
-    res.send('Hello World from Test');
-});
+// app.use( "/test", (req,res) =>{
+//     res.send('Hello World from Test');
+// });
 
 // app.use( "/hello", (req,res) =>{
 //     res.send('Hello World , Hello Hello hello');
