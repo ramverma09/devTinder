8ec61cf19ad49d8d2bb6d24470d3afba9944d411
ramverma09 <rv638069@gmail.com>
2026-06-01 00:04:43 +0530

    Exploring get, post , delete api calls

--- src/app.js
+++ src/app.js
@@ -2,17 +2,31 @@ const express  = require('express');
 
 const app = express();
 
-app.use( "/", (req,res) =>{
-    res.send('Hello World and Namaste from  Dashboard');
+
+app.get("/user", (req,res) =>{
+    res.send({"firstName": "Ram", "lastName": "Verma"});
 });
 
-app.use( "/hello", (req,res) =>{
-    res.send('Hello World , Hello Hello hello');
+app.post("/user", (req,res) =>{
+    // Saved the data to database
+    res.send("Data saved successfully to the database");
+});
+
+app.delete("/user", (req,res) =>{
+    // Delete the data from database
+    res.send("Data deleted successfully from the database");
 });
 
 app.use( "/test", (req,res) =>{
     res.send('Hello World from Test');
 });
+
+// app.use( "/hello", (req,res) =>{
+//     res.send('Hello World , Hello Hello hello');
+// });
+// app.use( "/", (req,res) =>{
+//     res.send('Hello World and Namaste from  Dashboard');
+// });
 
 app.listen(7777, ()=>{
     console.log('Server is running on port 7777');
