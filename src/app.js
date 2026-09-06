15d356f998f199ada7901c373e04030d1a536a73
ramverma09 <rv638069@gmail.com>
2026-09-06 19:15:48 +0530

    get api

--- src/app.js
+++ src/app.js
@@ -21,6 +21,31 @@ app.post("/signup", async (req,res)=>{
     }
 });
 
+
+app.get("/user",async (req,res)=>{
+    const userEmail =req.body.emailId;
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length ===0){
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(404).send("something went wrong");
+    }
+});
+
+// Feed Api - Get /feed -> get all the users data from the database
+app.get("/feed",async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(500).send("Error fetching user data: " + err.message);
+    } 
+});
+
 connectDB().then(() => {
     console.log("Database connected successfully");
     app.listen(7777, ()=>{
